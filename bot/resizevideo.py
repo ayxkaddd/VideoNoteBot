3e1dce40b1b506889b08510307d704ddbe42b7f1
мама я программист <82116858+ayxkaddd@users.noreply.github.com>
2022-08-30 01:39:17 +0300

    ooops

--- bot/resizevideo.py
+++ bot/resizevideo.py
@@ -8,7 +8,7 @@ class Resize:
 		self.path_to = path_to
 
 
-	def res_video(self):
+	def resize_video(self):
 		command = f"ffmpeg -i {self.path_from} -vf scale=460:460 {self.path_to}"
 		os.system(f"{command}")
 		os.system(f"rm {self.path_from}")
