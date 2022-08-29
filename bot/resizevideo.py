95324e1dde81bab2a16a7ab1e307a771afaddc1d
мама я программист <82116858+ayxkaddd@users.noreply.github.com>
2022-08-30 01:38:23 +0300

    putin still hyilo

--- bot/resizevideo.py
+++ bot/resizevideo.py
@@ -3,15 +3,12 @@ import os
 
 class Resize:
 
-	""""that is so easy, i just use ffmpeg
-	 and search at google how to resize video with ffmpeg lol"""
-
 	def __init__(self, path_from, path_to):
 		self.path_from = path_from
 		self.path_to = path_to
 
 
 	def res_video(self):
-		command = f'ffmpeg -i {self.path_from} -vf scale=460:460 {self.path_to}'
-		os.system(f'{command}')
-		os.system(f'rm {self.path_from}')
+		command = f"ffmpeg -i {self.path_from} -vf scale=460:460 {self.path_to}"
+		os.system(f"{command}")
+		os.system(f"rm {self.path_from}")
