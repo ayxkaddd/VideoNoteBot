b1eabf5f4d72270b41a4eb7daa58b24b29ff1463
x <zxcvbqwert390@gmail.com>
2023-05-29 18:26:20 +0300

    rewrited old code brah i was stupid asf

--- bot/main.py
+++ bot/main.py
@@ -1,12 +1,15 @@
-import telebot
 import os
+import telebot
 from resizevideo import Resize
 
 
 token = '' # <--- bot token here
 
 bot = telebot.TeleBot(token)
+resize = Resize()
 
+save_dir = 'video'
+out_dir = 'output'
 
 @bot.message_handler(commands=['start'])
 def start(message):
@@ -20,64 +23,45 @@ def help(message):
 	bot.send_message(message.chat.id, 'just send me some video or gif and you can see the result')
 
 
-@bot.message_handler(content_types=['video'])
+@bot.message_handler(content_types=['video', 'animation'])
 def video_note(message):
 	try:
-		save_dir = 'video'
-		file_id_info = bot.get_file(message.video.file_id)
-		bot.send_chat_action(message.chat.id, 'typing')
-		sent_message = bot.send_message(message.chat.id, 'download file...')
-		downloaded_file = bot.download_file(file_id_info.file_path)
 		src = f'{message.chat.id}.mp4'
+		
+		try:
+			file_id_info = bot.get_file(message.video.file_id)
+			print("File is video")
+		except:
+			file_id_info = bot.get_file(message.animation.file_id)
+			print("File is animation")
+		
+		bot.send_chat_action(message.chat.id, 'typing')
+		sent_message = bot.send_message(message.chat.id, 'downloading file...')
+		
+		downloaded_file = bot.download_file(file_id_info.file_path)
+		
 		# save file to dir
-		with open(f'{save_dir}/{src}', 'wb') as new_file:
-			new_file.write(downloaded_file)
+		with open(f'{save_dir}/{src}', 'wb') as file:
+			file.write(downloaded_file)
 
 		bot.send_chat_action(message.chat.id, 'typing')
-		bot.edit_message_text('resize video...', message.chat.id, message_id=sent_message.message_id)
-		out_dir = f'output/{src}'
-		# resize this file
-		resize = Resize(path_from=f'{save_dir}/{src}', path_to=out_dir)
-		resize.resize_video()
-		path = f'output/{message.chat.id}.mp4'
-		bot.edit_message_text('sends video note...', message.chat.id, message_id=sent_message.message_id)
-		file = open(path, 'rb')
+		bot.edit_message_text('resizing video...', message.chat.id, message_id=sent_message.message_id)
+		
+		# resize file
+		resize.resize_video(path_from=f'{save_dir}/{src}', path_to=f"{out_dir}/{src}")
+
+		bot.edit_message_text('sending video note...', message.chat.id, message_id=sent_message.message_id)
 		bot.send_chat_action(message.chat.id, 'record_video')
-		bot.send_video_note(message.chat.id, file)
+		bot.send_video_note(message.chat.id, open(f"output/{message.chat.id}.mp4", 'rb'))
+		
 		# remove file
-		os.system(f'rm {path}')
+		
+		os.system(f'rm {out_dir}/{src}')
+		
 		bot.edit_message_text('Done!', message.chat.id, message_id=sent_message.message_id)
 	except Exception as ex:
 		print(f'VIDEO NOTE EXCPET\n{ex=}')
-
-
-@bot.message_handler(content_types=['animation'])
-def video_note_animation(message):
-	try:
-		save_dir = 'video'
-		file_id_info = bot.get_file(message.animation.file_id)
-		bot.send_chat_action(message.chat.id, 'typing')
-		sent_message = bot.send_message(message.chat.id, 'download file...')
-		downloaded_file = bot.download_file(file_id_info.file_path)
-		src = f'{message.chat.id}.mp4'
-
-		with open(f'{save_dir}/{src}', 'wb') as new_file:
-			new_file.write(downloaded_file)
-
-		bot.send_chat_action(message.chat.id, 'typing')
-		bot.edit_message_text('resize video...', message.chat.id, message_id=sent_message.message_id)
-		out_dir = f'output/{src}'
-		resize = Resize(path_from=f'{save_dir}/{src}', path_to=out_dir)
-		resize.resize_video()
-		path = f'output/{message.chat.id}.mp4'
-		bot.edit_message_text('sends video note...', message.chat.id, message_id=sent_message.message_id)
-		file = open(path, 'rb')
-		bot.send_chat_action(message.chat.id, 'record_video')
-		bot.send_video_note(message.chat.id, file)
-		os.system(f'rm {path}')
-		bot.edit_message_text('Done!', message.chat.id, message_id=sent_message.message_id)
-	except Exception as ex:
-		print(f'VIDEO NOTE EXCPET\n{ex=}')
+		bot.send_message(message.chat.id, "Something went wrong. Please try again later.")
 
 
 bot.polling(none_stop=True, interval=0)
--- bot/resizevideo.py
+++ bot/resizevideo.py
@@ -3,12 +3,12 @@ import os
 
 class Resize:
 
-	def __init__(self, path_from, path_to):
-		self.path_from = path_from
-		self.path_to = path_to
+	# def __init__(self, path_from, path_to):
+	# 	self.path_from = path_from
+	# 	self.path_to = path_to
 
 
-	def resize_video(self):
-		command = f"ffmpeg -i {self.path_from} -vf scale=460:460 {self.path_to}"
+	def resize_video(self, path_from, path_to):
+		command = f"ffmpeg -i {path_from} -vf scale=460:460 {path_to}"
 		os.system(f"{command}")
-		os.system(f"rm {self.path_from}")
+		os.system(f"rm {path_from}")
